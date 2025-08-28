e8ca88182c9769d9a701fcb6a471a13c6093eae3
saiter <saiter45@mail.ru>
2025-08-28 18:52:03 +0500

    Добавил элементы покупки

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
+import React from "react"
 import Header from "./components/header"
 import Footer from "./components/footer"
+import Items from './components/items'
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Footer />        
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Куртка серая',
+          img: 'grey-jacket.jpg',
+          desc: 'Lorem',
+          category: 'winter',
+          price: '29.99'
+        },
+        {
+          id: 2,
+          title: 'Куртка синяя',
+          img: 'blue-jacket.png',
+          desc: 'Lorem',
+          category: 'winter',
+          price: '25.99'
+        },
+        {
+          id: 3,
+          title: 'Штаны серые',
+          img: 'grey-trousers.webp',
+          desc: 'Lorem',
+          category: 'pants',
+          price: '19.50'
+        },
+        {
+          id: 4,
+          title: 'Ботинки черные',
+          img: 'black-boots.jpg',
+          desc: 'Lorem',
+          category: 'shoes',
+          price: '14.99'
+        }
+      ]
+    }
+  }
+  render(){
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />        
+      </div>
+    )
+  }
 }
 
 export default App;
